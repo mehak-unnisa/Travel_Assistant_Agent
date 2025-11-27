5993d2e24f749f0a5547bdf24d367bdb0547de2c
mehak-unnisa <147539128+mehak-unnisa@users.noreply.github.com>
2025-11-27 16:36:52 +0400

    Add files via upload

---
+++ Travel-Assistant-ADK/my_agent/agent.py
@@ -0,0 +1,16 @@
+from google.adk.agents.llm_agent import Agent
+from my_agent.supporting_agents import travel_inspiration_agent
+from google.genai import types
+
+root_agent = Agent(
+    model="gemini-2.5-flash",
+    name="travel_planner_main",
+    description="A helpful travel planning assistant that helps users plan their trips by providing information and suggestions based on their preferences.",
+    instruction="""
+            - You are an exclusive travel concierge agent
+            - You help users to discover their dream holiday destination and plan their vacation.
+            - Use the inspiration_agent to get the best destination, news, places nearby e.g hotels, cafes, etc near attractions and points of interest for the user.
+            - You cannot use any tool directly. 
+            """,
+    sub_agents=[travel_inspiration_agent],
+)
